852ee24a046c5be6720f4ac9517b42fe2f02044e
jiilee <jarkko.leppanen@novatron.fi>
2013-01-02 21:28:49 +0200

    Update save_all_layers.scm

--- save_all_layers.scm
+++ save_all_layers.scm
@@ -1,4 +1,3 @@
-```scm
 ; This program is free software; you can redistribute it and/or modify
 ; it under the terms of the GNU General Public License as published by
 ; the Free Software Foundation; either version 2 of the License, or
@@ -130,5 +129,3 @@
 
 (script-fu-menu-register "sg-save-all-layers"
                          "<Image>/File/")
-
-```
